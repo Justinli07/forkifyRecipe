d05f77e1e7e71d921f24ec1d03d4755874553e51
Justinli07 <justinli70777@gmail.com>
2021-12-13 22:40:33 -0800

    inital commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e4648115-8be4-4ee6-ae12-f6c83de33c77';
+export const MODAL_CLOSE_SEC = 2.5;
